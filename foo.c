a0e64d8ecd23ed22a751c57e3481b03921bae967
vitorscassiano <virjcas@gmail.com>
2017-02-16 02:07:22 -0200

    Some files

---
+++ foo.c
@@ -0,0 +1,28 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  foo.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  05/01/2017 01:31:37
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char *members;
+    memset(members, 0, 255);
+
+    printf("%c",members);
+
+}
